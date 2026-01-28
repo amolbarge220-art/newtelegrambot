d679a8752437a41299eacce89800727657c0afe8
amolbarge220-art <amolbarge220@gmail.com>
2026-01-28 19:44:23 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+from telegram.ext import Updater, CommandHandler
+
+TOKEN = "YOUR_BOT_TOKEN"
+
+def start(update, context):
+    update.message.reply_text("Bot running")
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+
+    updater.start_polling()
+    updater.idle()
